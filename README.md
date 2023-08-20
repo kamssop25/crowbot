0a669cec7b9340e43e6a772f81a6f475a3da757f
!  "Kamss <97346255+kamssop25@users.noreply.github.com>
2023-08-20 22:26:52 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# crowbot
+# naoki-yoko-perso
+Hey, version qui marche du naoki
+
+# Instalation
+
+-faite `npm i` dans le cmd ou le terminal
+-faite `node index.js` **dans le terminal pour start le bot**
+
+# Code à
+https://github.com/Betaaaaaaaaaaa/naoki-yoko-perso/
+
+# Contact Me
+
+Discord : !salah#2200
---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    app: {
+        px: '+',
+        token: '',
+        owners: '898999050320363550',
+        funny: '898999050320363550',
+        color: 'RANDOM',
+        footer: 'salahDev',
+        maxserver: '6',
+        everyoneMention: false,
+        hostedBy: true,
+        }
+    }
